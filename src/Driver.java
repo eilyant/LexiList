e5d56ef2f98f6c61c9367bb272ab6e5cb6141fa2
Eilya Nasertorabi <ilia.nasertorabi@gmail.com>
2024-11-08 13:51:48 -0500

    Update Driver.java

--- src/Driver.java
+++ src/Driver.java
@@ -1,12 +1,3 @@
-
-/**
- * <p>Name and ID: Eilya Nasertorabi - 40183363</p>
- * <p>Course: COMP249</p>
- * <p>Assignment #: 3</p>
- * <p>Due Date: April 15th</p>
- * 
- */
-
 import java.util.*;
 
 import java.io.*;
